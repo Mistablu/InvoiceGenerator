5306314c9766ec67e59ae055f777c6f3ccf53aaf
Morgan <m.garricks@lancaster.ac.uk>
2024-10-30 12:50:28 +0000

    added total sum

--- main.py
+++ main.py
@@ -38,5 +38,23 @@ for filepath in filepaths:
         pdf.cell(w=32,h=8,txt=str(row["amount_purchased"]),border=1)
         pdf.cell(w=29,h=8,txt=str(row["price_per_unit"]),border=1)
         pdf.cell(w=29,h=8,txt=str(row["total_price"]),border=1,ln=1)
+    
+    total_price = str(df["total_price"].sum())
+
+    pdf.set_font(family="Times",size=10)
+    pdf.set_text_color(80,80,80)
+    pdf.cell(w=30,h=8, border=1)
+    pdf.cell(w=70,h=8, border=1)
+    pdf.cell(w=32,h=8, border=1)
+    pdf.cell(w=29,h=8, border=1)
+    pdf.cell(w=29,h=8, txt=total_price, border=1, ln=1)
+
+    pdf.set_text_color(0,0,0)
+    pdf.set_font(family="Times",size=12,style = "B")
+    pdf.cell(w=30,h=8,txt=f"The total price is: {total_price}", ln=1)
+
+    pdf.set_font(family="Times",size=15, style = "B")
+    pdf.cell(w=40,h=8,txt=f"Made in Python")
+    pdf.image("pythonhow.png",w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
